1f2d7c1b5507657cb18043a90d5c3711a052cfc0
Timothy Rice <timothy.rice@unimelb.edu.au>
2018-10-26 21:19:15 +1100

    Simplify config

--- config.h
+++ config.h
@@ -23,25 +23,12 @@ static       Bool urgentswitch  = False;
 static int  newposition   = -1;
 static Bool npisrelative  = False;
 
-#define SETPROP(p) { \
-        .v = (char *[]){ "/bin/sh", "-c", \
-                "prop=\"`xwininfo -children -id $1 | grep '^     0x' |" \
-                "sed -e's@^ *\\(0x[0-9a-f]*\\) \"\\([^\"]*\\)\".*@\\1 \\2@' |" \
-                "xargs -0 printf %b | dmenu -l 10 -w $1`\" &&" \
-                "xprop -id $1 -f $0 8s -set $0 \"$prop\"", \
-                p, winid, NULL \
-        } \
-}
-
 #define MODKEY ControlMask
 static Key keys[] = {
 	/* modifier             key        function     argument */
-	{ MODKEY|ShiftMask,             XK_Return, focusonce,      { 0 } },
-	{ MODKEY,                       XK_t,      spawn,          { 0 } },
-	{ MODKEY,                       XK_p,      spawn,          SETPROP("_TABBED_SELECT_TAB") },
-	{ MODKEY,                       XK_Tab,    rotate,         { .i = +1 } },
-	{ MODKEY|ShiftMask,             XK_Tab,    rotate,         { .i = -1 } },
-	{ MODKEY,                       XK_h,      movetab,        { .i = -1 } },
-	{ MODKEY,                       XK_l,      movetab,        { .i = +1 } },
-    { MODKEY,                       XK_F11,    fullscreen,     { 0 } },
+	{ MODKEY,               XK_t,      spawn,       { 0 } },
+	{ MODKEY,               XK_Tab,    rotate,      { .i = +1 } },
+	{ MODKEY|ShiftMask,     XK_Tab,    rotate,      { .i = -1 } },
+	{ MODKEY,               XK_h,      movetab,     { .i = -1 } },
+	{ MODKEY,               XK_l,      movetab,     { .i = +1 } },
 };
